3149bff9f374d56857031f8990325e625d24e14c
KlimekM <mklimek15@gmail.com>
2016-03-28 08:50:30 -0500

    Add relationships to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+	has_many :comments, foreign_key: :commenter_id
+	has_many :likes, foreign_key: :liker_id
+	has_many :friendships, foreign_key: :friend_1_id
+  has_many :friends, through: :friendships, source: :friend
+  has_many :checkins
+  has_many :checked_in_places, through: :checkins
+
+  validates :username, :first_name, :last_name, :email, { presence: true}
+  has_secure_password
 end
