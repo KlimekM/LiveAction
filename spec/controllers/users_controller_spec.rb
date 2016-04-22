3d9cc3533d3ea905c1aff1534b092c8fa9b1fd48
KlimekM <mklimek15@gmail.com>
2016-04-21 21:38:56 -0500

    Write tests for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,7 +17,22 @@ describe UsersController do
 				expect(assigns(:user)).to be_a(User)
 			end
 
-			it "assigns session[:user_id] to the new user's id if the user is saved" do
+			# Ask about testing this!
+			# it "assigns session[:user_id] to the new user's id if the user is saved" do
+			# 	post :create, user: { first_name: user.first_name, last_name: user.last_name, username: user.username, email: user.email, password: user.password}
+			# 	expect(assigns(session[:user_id])).to eq(user.id)
+			# end
+
+			it "re-renders the login page if the username or password are invalid" do
+				post :create, user: { first_name: user.first_name, last_name: user.last_name, username: user.username, email: user.email, password: user.password}
+				expect(response).to render_template(:show)
+			end
+		end
+
+		context "when invalid params are passed" do
+			it "re-renders the registration page if the e-mail is left empty" do
+				post :create, user: { first_name: user.first_name, last_name: user.last_name, username: user.username, email: "", password: user.password}
+				expect(response).to render_template(:new)
 			end
 		end
 	end
