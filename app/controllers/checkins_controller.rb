bf6f1865135bb4a94b1cf07948667bb4c351da9d
KlimekM <mklimek15@gmail.com>
2016-04-27 10:36:06 -0500

    Fix associations, add checkin form

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,8 +1,11 @@
 class CheckinsController < ApplicationController
 	def new
 		@place = Place.find(params[:place_id])
+		@checkin = Checkin.new
 	end
 
 	def create
+		@place = Place.find(params[:place_id])
+		@checkin = Checkin.new()
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	has_many :friendships, foreign_key: :friend_1_id
   has_many :friends, through: :friendships, source: :friend
   has_many :checkins
-  has_many :visited_places, through: :checkins, class_name: "Place"
+  has_many :visited_places, through: :checkins, source: :place
 
   validates :username, :first_name, :last_name, :email, { presence: true}
   has_secure_password
