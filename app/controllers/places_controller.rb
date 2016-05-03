631a9e4aa257e3b07a7f1df93be530bc42c9c9ae
KlimekM <mklimek15@gmail.com>
2016-05-03 10:37:17 -0500

    Modify places controller and show page to safe guard against incorrect urls

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,6 +4,12 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find(params[:id])
+    @place = Place.find_by_id(params[:id])
+    if @place
+      render "show"
+    else
+      flash[:notice] = "The place that you requested does not exist."
+      render "show"
+    end
   end
 end
