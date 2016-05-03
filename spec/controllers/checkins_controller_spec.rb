a55a87c6fdc24d857c31d0963ecbe78bf6ed569b
KlimekM <mklimek15@gmail.com>
2016-05-03 17:00:23 -0500

    Add tests for the checkins show action

--- spec/controllers/checkins_controller_spec.rb
+++ spec/controllers/checkins_controller_spec.rb
@@ -53,20 +53,56 @@ describe CheckinsController do
       let!(:checkin) { FactoryGirl.build :checkin, place_id: place.id, user_id: user.id }
       it "assigns @place to the correct instance of Place" do
         session[:user_id] = user.id
-        post :create, user_id: user.id, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
+        post :create, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
         expect(assigns(:place).id).to eq(place.id)
       end
 
       it "adds a checkin to @user.checkins" do
         session[:user_id] = user.id
-        post :create, user_id: user.id, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
+        post :create, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
         expect(assigns(:user).checkins.count).to eq(user.checkins.count)
       end
 
       it "renders the checkins index page when the checkin saves" do
         session[:user_id] = user.id
-        post :create, user_id: user.id, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
+        post :create, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
         expect(response).to render_template(:index)
+      end
+    end
+
+    context "when invalid params are passed" do
+      # Having a difficult time, breaking code to get test to pass.
+      let!(:place) { FactoryGirl.create :place }
+      let!(:user) { FactoryGirl.create :user }
+      let!(:checkin) { FactoryGirl.build :checkin, place_id: place.id, user_id: user.id }
+      it "re-renders the new checkin template" do
+        session[:user_id] = user.id
+        post :create, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "GET #show" do
+    context "when a place and checkin exist" do
+      let!(:place) { FactoryGirl.create :place }
+      let!(:checkin) { FactoryGirl.create :checkin }
+      let!(:user) { FactoryGirl.create :user, id: checkin.user_id }
+      it "assigns @place, @checkin, and @user to the correct place, checkin, and user" do
+        get :show, place_id: place.id, id: checkin.id
+        expect(assigns(:place).address).to eq(place.address)
+        expect(assigns(:checkin).description).to eq(checkin.description)
+        expect(assigns(:user).id).to eq(checkin.user_id)
+      end
+    end
+
+    context "when a place or checkin do not exist" do
+      let!(:place) { FactoryGirl.create :place }
+      let!(:checkin) { FactoryGirl.create :checkin }
+      let!(:user) { FactoryGirl.create :user, id: checkin.user_id }
+      it "assigns 'The place or checkin that you requested does not exist.' as a flash[:notice]" do
+        get :show, place_id: place.id, id: Faker::Number.number(5)
+        expect(flash[:notice]).to eq("The place or checkin that you requested does not exist.")
       end
     end
   end
