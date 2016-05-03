a0341bac4f6c150e264859a3917aa27b33e142a2
KlimekM <mklimek15@gmail.com>
2016-05-03 09:01:33 -0500

    Begin writing tests for places_controller

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe PlacesController do
   describe "GET #index" do
     it "properly assigns all of the places to @places" do
-
+      places = FactoryGirl.create_list(:place, 3)
+      get :index
+      expect(assigns(:places).length).to eq(3)
     end
   end
 end
--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name { Faker::Name.name }
     description { Faker::Lorem.sentence }
     address { Faker::Address.street_address }
-    capacity { Faker::Number.number }
+    capacity { Faker::Number.number(5) }
     date_opened { Faker::Date.backward(2000) }
     image_url { Faker::Avatar.image }
   end
