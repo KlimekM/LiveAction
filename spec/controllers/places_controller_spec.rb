c2536df0f0f61f5092b082ac409ab8cc69eb622c
KlimekM <mklimek15@gmail.com>
2016-05-03 09:51:57 -0500

    Add tests to places controller

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -3,32 +3,24 @@ require 'spec_helper'
 describe PlacesController do
   describe "GET #index" do
     it "properly assigns all of the places to @places" do
-      # places = FactoryGirl.create_list(:place, 3)
-      # p "**************************"
-      # p places
-      # p places.length
-      # p "******************************"
+      places = FactoryGirl.create_list(:place, 3)
       get :index
       expect(assigns(:places).length).to eq(3)
     end
   end
 
-  # describe "GET #show" do
-  #   context "when a place exists" do
-  #     it "assigns @place as an instance of Place" do
-  #       place = FactoryGirl.create(:place)
-  #       p "++++++++++++++++++++++++++"
-  #       p place
-  #       p places.length
-  #       p "++++++++++++++++++++++++++"
-  #       get :show, id: place.id
-  #       expect(assigns(:place)).to be_a(Place)
-  #     end
+  describe "GET #show" do
+    context "when a place exists" do
+      let!(:place) { FactoryGirl.create :place }
+      it "assigns @place as an instance of Place" do
+        get :show, id: place.id
+        expect(assigns(:place)).to be_a(Place)
+      end
 
-  #     it "ensures @place has the correct id" do
-  #       get :show, id: place.id
-  #       expect(assigns(:place).id).to eq(place.id)
-  #     end
-  #   end
-  # end
+      it "ensures @place has the correct id" do
+        get :show, id: place.id
+        expect(assigns(:place).id).to eq(place.id)
+      end
+    end
+  end
 end
