1ec392ed723d2cff1c6fae9b7c79bcd792c7fd05
KlimekM <mklimek15@gmail.com>
2016-05-12 16:52:08 -0500

    Commit likes_controller_spec progress, return to creating a like to remove

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -2,6 +2,25 @@ require 'spec_helper'
 
 describe LikesController do
   describe "POST #create" do
+    # Throwing an error on line 10 here!
+    let(:user) { FactoryGirl.create :user }
+    let(:checkin) { FactoryGirl.create :checkin }
+    it "creates a like linked to the correct liker" do
+      session[:user_id] = user.id
+      post :create, checkin_id: checkin.id
+      expect(Like.last.liker_id).to eq(user.id)
+    end
+  end
 
+  describe "DELETE #destroy" do
+    let(:user) { FactoryGirl.create :user }
+    let(:checkin) { FactoryGirl.create :checkin }
+    # Create a like to remove
+    it "deletes a like" do
+      session[:user_id] = user.id
+      post :create, checkin_id: checkin.id
+      delete :destroy
+      expect(Like.find_by(liker_id: user.id)).to be nil
+    end
   end
 end
