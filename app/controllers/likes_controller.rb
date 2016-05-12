36184cfc64d750cba593efb48ab97f9d5600adac
KlimekM <mklimek15@gmail.com>
2016-05-12 15:46:03 -0500

    Rename votes to likes, delete a like on the checkins/index page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,17 @@
-class VotesController < ApplicationController
+class LikesController < ApplicationController
   def create
     @user = User.find_by_id(session[:user_id])
     @checkin = Checkin.find_by_id(params[:checkin_id])
     @place = @checkin.place
     @user.likes.create!(liker_id: session[:user_id], likeable_id: @checkin.id, likeable_type: "Checkin")
+    redirect_to [@place, @checkin]
+  end
+
+  def destroy
+    like = Like.find_by(liker_id: params[:user_id], likeable_id: params[:checkin_id])
+    like.destroy
+    @checkin = Checkin.find_by_id(params[:checkin_id])
+    @place = @checkin.place
     redirect_to [@place, @checkin]
   end
 end
