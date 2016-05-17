613569f6d97388c51ee5b0709bf2edfd55e8f565
KlimekM <mklimek15@gmail.com>
2016-05-17 10:14:27 -0500

    Add functionality to add/delete a friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,17 @@
 class FriendshipsController < ApplicationController
   def create
+    current_user = User.find_by_id(session[:user_id])
+    @user = User.find_by_id(params[:id])
+    current_user.friends << @user
+    @user.friends << current_user
+    redirect_to @user
   end
 
   def destroy
+    current_user = User.find_by_id(session[:user_id])
+    @user = User.find_by_id(params[:id])
+    current_user.friends.delete(@user)
+    @user.friends.delete(current_user)
+    redirect_to @user
   end
 end
