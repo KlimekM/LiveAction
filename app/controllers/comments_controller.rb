3341b7387350469bf7393bdd1919819187a9f05d
KlimekM <mklimek15@gmail.com>
2016-05-17 14:10:54 -0500

    Add functionality to comment on a checkin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
 class CommentsController < ApplicationController
   def create
     @checkin = Checkin.find_by_id(params[:checkin_id])
-    redirect_to @checkin
+    @place = @checkin.place
+    @comment = Comment.create!(commenter_id: session[:user_id], commentable_type: "Checkin", commentable_id: @checkin.id, text: params[:comment][:text])
+    # change this to if @... .save, else render
+    redirect_to [@place, @checkin]
   end
 end
