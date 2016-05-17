6afe0cf3ad48ccccd93acbf50ea5deb6f4d0acca
KlimekM <mklimek15@gmail.com>
2016-05-17 10:38:00 -0500

    Create tests for friendship deletion

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -24,5 +24,27 @@ describe FriendshipsController do
   end
 
   describe "DELETE #destroy" do
+    let(:user) { FactoryGirl.create :user }
+    let(:user_two) { FactoryGirl.create :user }
+    it "deletes user_two as a friend of user" do
+      session[:user_id] = user.id
+      post :create, id: user_two.id
+      delete :destroy, id: user_two.id
+      expect(user.friends.last).to_not eq(user_two)
+    end
+
+    it "deletes user as a friend of user_two" do
+      session[:user_id] = user.id
+      post :create, id: user_two.id
+      delete :destroy, id: user_two.id
+      expect(user_two.friends.last).to_not eq(user)
+    end
+
+    it "redirects to the appropriate user's profile" do
+      session[:user_id] = user.id
+      post :create, id: user_two.id
+      delete :destroy, id: user_two.id
+      expect(response).to redirect_to(user_two)
+    end
   end
 end
