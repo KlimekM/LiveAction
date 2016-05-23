efab16c6bf96964fd605ce67f54ffda24e209b7b
KlimekM <mklimek15@gmail.com>
2016-05-23 13:49:50 -0500

    Finally get tests to pass for likes_contoller

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -6,5 +6,12 @@ FactoryGirl.define do
     capacity { Faker::Number.number(5) }
     date_opened { Faker::Date.backward(2000) }
     image_url { Faker::Avatar.image }
+
+    factory :place_with_checkin do
+      after(:create) do |place|
+        create(:checkin, place: place)
+      end
+    end
   end
+
 end
