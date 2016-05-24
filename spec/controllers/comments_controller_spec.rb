1ee3f8a125c7f150210fa12f2f10e4cd7602ce15
KlimekM <mklimek15@gmail.com>
2016-05-24 07:22:05 -0500

    Fix checkin factory to include user and place associations

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe CommentsController do
+  describe "POST #create" do
+    it "creates a new instance of a Comment" do
+
+    end
+  end
+end
--- spec/factories/checkins.rb
+++ spec/factories/checkins.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :checkin do
-    user_id { Faker::Number.between(1, 100) }
-    place_id { Faker::Number.between(1, 100) }
+    user
+    place
     date_attended { Faker::Date.backward(365) }
     image_url { Faker::Avatar.image }
     description { Faker::Lorem.sentence }
