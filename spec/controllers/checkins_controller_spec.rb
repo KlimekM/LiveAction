30abd6439f4067fbe4b083ecf4abc71bf2c752c2
KlimekM <mklimek15@gmail.com>
2016-06-05 22:07:39 -0500

    Fix test for checkins#show, write tests for checkins#update when valid params are passed

--- spec/controllers/checkins_controller_spec.rb
+++ spec/controllers/checkins_controller_spec.rb
@@ -107,16 +107,37 @@ describe CheckinsController do
   end
 
   describe "PUT #update" do
-    # WRITE TESTS HERE
+    let(:place) { FactoryGirl.create :place }
+    let(:user) { FactoryGirl.create :user }
+    let(:checkin) { FactoryGirl.create :checkin, place_id: place.id, user_id: user.id }
+    context "when valid params are passed" do
+      it "assigns @place and @checkin as the correct instance of each model" do
+        new_description = Faker::Lorem.sentence
+        get :edit, place_id: place.id, id: checkin.id
+        put :update, place_id: checkin.place.id, id: checkin.id, checkin: { description: new_description, place_id: checkin.place_id, "date_attended(1i)"=>"2016", "date_attended(2i)"=>"5", "date_attended(3i)"=>"30" }
+        expect(assigns(:place).id).to eq(place.id)
+        expect(assigns(:checkin).id).to eq(checkin.id)
+      end
+
+      it "changes the description of the checkin to new_description" do
+        new_description = Faker::Lorem.sentence
+        get :edit, place_id: place.id, id: checkin.id
+        put :update, place_id: checkin.place.id, id: checkin.id, checkin: { description: new_description, place_id: checkin.place_id, "date_attended(1i)"=>"2016", "date_attended(2i)"=>"5", "date_attended(3i)"=>"30" }
+        expect(assigns(:checkin).description).to eq(new_description)
+      end
+    end
+
+    context "when invalid params are passed" do
+    end
   end
 
   describe "GET #show" do
     context "when a place and checkin exist" do
       let(:place) { FactoryGirl.create :place }
       let(:user) { FactoryGirl.create :user }
-      let(:checkin) { FactoryGirl.create :checkin }
+      let(:checkin) { FactoryGirl.create :checkin, place_id: place.id, user_id: user.id }
       it "assigns @place, @checkin, and @user to the correct place, checkin, and user" do
-        get :show, place_id: place.id, id: checkin.id
+        get :show, place_id: checkin.place.id, id: checkin.id
         expect(assigns(:place).address).to eq(place.address)
         expect(assigns(:checkin).description).to eq(checkin.description)
         expect(assigns(:user).id).to eq(checkin.user_id)
@@ -128,7 +149,7 @@ describe CheckinsController do
       let(:checkin) { FactoryGirl.create :checkin }
       let(:user) { FactoryGirl.create :user, id: checkin.user_id }
       it "assigns 'The place or checkin that you requested does not exist.' as a flash[:notice]" do
-        get :show, place_id: place.id, id: Faker::Number.number(5)
+        get :show, place_id: checkin.place.id, id: Faker::Number.number(5)
         expect(flash[:notice]).to eq("The place or checkin that you requested does not exist.")
       end
     end
@@ -147,7 +168,7 @@ describe CheckinsController do
 
     it "redirects to the correct place" do
       session[:user_id] = user.id
-      post :create, place_id: place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
+      post :create, place_id: checkin.place.id, checkin: { description: checkin.description, place_id: checkin.place_id, "date_attended(1i)"=>"2015", "date_attended(2i)"=>"5", "date_attended(3i)"=>"3" }
       delete :destroy, id: Checkin.last.id, place_id: place.id
       expect(response).to redirect_to("/places/#{place.id}/checkins")
     end
