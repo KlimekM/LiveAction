e028e1d9e4981e659636af4fe8e37ef195314eae
KlimekM <mklimek15@gmail.com>
2016-06-06 18:47:12 -0500

    Add current_user helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  
+  private
+
+  def current_user
+    User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def authorized(creator_id)
+    current_user && current_user.id == creator_id
+  end
 end
