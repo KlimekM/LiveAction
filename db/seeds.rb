f28a8aaf0ff1de7adce79f323d084f384f3dc4f6
KlimekM <mklimek15@gmail.com>
2016-06-12 22:02:53 -0500

    Begin adding place seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Place.create!(name: "United Center", description: "Home of the Bulls", address: "2000 W Madison Ave", capacity: 20000)
+Place.create!(name: "Air Canada Centre", description: "Home of the Toronto Raptors", address: "40 Bay Street", city: "Toronto", state: "Ontario", capacity: 19800, date_opened: DateTime.new(02, 19, 1999))
 
-Place.create!(name: "Oracle Arena", description: "Home of the Warriors", address: "1000 W Lake Ave", capacity: 25000)
+Place.create!(name: "American Airlines Arena", description: "Home of the Miami Heat", address: "601 Biscayne Blvd", city: "Miami", state: "Florida", capacity: 19600, date_opened: DateTime.new(12, 31, 1999))
 
-Place.create!(name: "Boston Garden", description: "Home of the Celtics", address: "500 W Green St", capacity: 18000)
+Place.create!(name: "American Airlines Center", description: "Home of the Dallas Mavericks", address: "2500 Victory Avenue", city: "Dallas", state: "Texas", capacity: 19200, date_opened: DateTime.new(09, 01, 1999))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Place.create!(name: "United Center", description: "Home of the Chicago Bulls", address: "2000 W Madison Ave", capacity: 20000)
+
+Place.create!(name: "Oracle Arena", description: "Home of the Golden State Warriors", address: "1000 W Lake Ave", capacity: 25000)
+
+Place.create!(name: "Boston Garden", description: "Home of the Boston Celtics", address: "500 W Green St", capacity: 18000)
