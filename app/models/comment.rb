997ef52702a23ce4a16c8a74986aefea6672dc2b
KlimekM <mklimek15@gmail.com>
2016-06-23 09:47:09 -0500

    Add a method to the comment model to extract logic out of view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ActiveRecord::Base
   belongs_to :commenter, class_name: "User"
 
   validates :text, presence: true
+
+  def comment_author?(current_user)
+    self.commenter_id == current_user.id
+  end
 end
