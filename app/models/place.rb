608402b4fd0f0ec2882f6ad7461d00d5762840fa
KlimekM <mklimek15@gmail.com>
2016-06-28 12:39:10 -0500

    Remove has_many relationship for likes and comments on places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,4 @@
 class Place < ActiveRecord::Base
-  has_many :likes, foreign_key: :likeable_id
-  has_many :comments
   has_many :checkins
   has_many :visitors, through: :checkins, source: :user
 end
